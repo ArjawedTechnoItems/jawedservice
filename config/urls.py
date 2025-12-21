b589240678bd804a7d0f6ccfc71236ed2e1e40eb
gargyogesh1 <106339179+gargyogesh1@users.noreply.github.com>
2025-12-21 23:58:01 +0530

    k

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("🎉 Django is running successfully on Render!")
 
 urlpatterns = [
+    path('', home), 
     path('admin/', admin.site.urls),
 ]
